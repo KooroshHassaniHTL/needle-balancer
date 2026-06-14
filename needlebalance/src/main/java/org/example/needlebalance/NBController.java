ee9a4cc989b34e69147785f41e0b169f12f47f7e
KooroshHassaniHTL <k.hassani@students.htl-leonding.ac.at>
2026-06-14 23:06:40 +0200

    added a progressbar and changed the score from a double to a double property so it is compatible with javafx property bindings

--- needlebalance/src/main/java/org/example/needlebalance/NBController.java
+++ needlebalance/src/main/java/org/example/needlebalance/NBController.java
@@ -1,10 +1,14 @@
 package org.example.needlebalance;
 
 import javafx.animation.AnimationTimer;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
@@ -15,25 +19,31 @@ import javafx.scene.shape.Rectangle;
 import java.util.Random;
 
 public class NBController {
+
     /*
-        @FXML private Button leftButton;
-        @FXML private Button rightButton;
-        @FXML private Rectangle backgroundBar;
-         */
+    @FXML private Button leftButton;
+    @FXML private Button rightButton;
+    @FXML private Rectangle backgroundBar;
+    @FXML private Pane balancePane;
+    */
+
     @FXML private Button startButton;
     @FXML private Line balanceMarker;
     @FXML private Rectangle safeZone;
-    @FXML public Label scoreLabel;
-    @FXML public Pane balancePane;
+    @FXML private Label scoreLabel;
+    @FXML private ProgressBar scoreProgressBar;
     @FXML private AnchorPane scene;
-    private int score = 0;
+
+    //private double score = 0;
+    private final DoubleProperty scoreProperty = new SimpleDoubleProperty(0);
     private boolean leftHold = false;
     private boolean rightHold = false;
     private boolean randomizePosition = false;
-    private double needleX = BAR_WIDTH/2; // start position in the middle of bar
+    private double needleX = BAR_WIDTH/2; // needle start pos
     public static final double BAR_WIDTH = 500; // if you change this also change fxml accordingly
-    private static final double NEEDLE_TICKSPEED = 0.2;
-    private static final int RANDOMIZER_MODIFIER = 4;
+    private static final double NEEDLE_TICKSPEED = 0.2; // how fast the player can move the needle
+    private static final int RANDOMIZER_MODIFIER = 4; // how badly the needle gets moved by randomization
+    private static final double POINT_GAIN_MODIFIER = 0.0002;
 
     private AnimationTimer animationTimer;
 
@@ -85,6 +95,9 @@ public class NBController {
            }
         });
 
+        scoreProgressBar.progressProperty().bind(scoreProperty);
+
+
         animationTimer = new AnimationTimer() {
             @Override
             public void handle(long now){
@@ -101,9 +114,11 @@ public class NBController {
                     if (randomizer < 0.5) moveNeedle(posRandom);
                     else moveNeedle(negRandom);
                 }
-                if (isInSafeZone() && randomizePosition){
-                    score += 1;
-                    scoreLabel.setText("Score: " + score);
+                if (isInSafeZone() && randomizePosition && scoreProperty.get() < 1.0){
+                    // score += POINT_GAIN_MODIFIER;
+                    //scoreLabel.setText("Score: " + Math.round(score) + "%");
+                    scoreProperty.set(scoreProperty.get() + POINT_GAIN_MODIFIER);
+                    scoreLabel.setText("Score: " + Math.round(scoreProperty.get()*100) + "%");
                 }
             }
         };
