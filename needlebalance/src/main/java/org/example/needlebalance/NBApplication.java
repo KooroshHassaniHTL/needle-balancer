ffaf9ad175ee3d6048b5be0e19126fd4415f45af
KooroshHassaniHTL <k.hassani@students.htl-leonding.ac.at>
2026-06-18 09:23:40 +0200

    styling exactly how I imagined

--- needlebalance/src/main/java/org/example/needlebalance/NBApplication.java
+++ needlebalance/src/main/java/org/example/needlebalance/NBApplication.java
@@ -12,7 +12,7 @@ public class NBApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NBApplication.class.getResource("needlebalance-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 700);
-        stage.setTitle("Hello!");
+        stage.setTitle("Raging River!");
         stage.setScene(scene);
         stage.show();
     }
